36c6d194f2bdcd01803fbfbe8d10f268426b3d47
Karan Bhardwaj <karanbhardwaj0077@gmail.com>
2026-09-10 02:40:14 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:   "https://dev-collab-developer-collabaration-nine.vercel.app/",
+  baseURL:   "https://devcollab-developer-collabaration-hub.onrender.com/";
   headers: {
     "Content-Type": "application/json",
   },
