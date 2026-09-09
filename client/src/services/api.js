28e6064fe0713e6533ea66ada03ba0498add652f
Karan Bhardwaj <karanbhardwaj0077@gmail.com>
2026-09-10 02:41:14 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:   "https://devcollab-developer-collabaration-hub.onrender.com/";
+  baseURL:   "https://devcollab-developer-collabaration-hub.onrender.com/",
   headers: {
     "Content-Type": "application/json",
   },
