4a8b511b079db408295791862e7dc1685f146ede
Karan Bhardwaj <karanbhardwaj0077@gmail.com>
2026-09-10 02:21:42 +0530

    improved code

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:   "http://localhost:5000/api" || "https://dev-collab-developer-collabaration-nine.vercel.app",
+  baseURL:   "http://localhost:5000/api" || "https://dev-collab-developer-collabaration-nine.vercel.app/",
   headers: {
     "Content-Type": "application/json",
   },
